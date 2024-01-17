e80c050ff340da4b93238ad20bc471772d3de054
apodgornyitba <apodgorny@itba.edu.ar>
2024-01-17 15:53:24 -0300

    Feat: Background styling done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import './theme.css';
+import hillsBG from './images/pattern-hills.svg';
+import starsBG from './images/bg-stars.svg';
+import Counter from "./components/Counter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const hillsBGStyle = {
+        backgroundImage: `url(${hillsBG})`,
+        backgroundColor: 'inherit',
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center bottom",
+        display: "flex",
+        alignItems: "flex-end",
+        justifyContent: "center",
+        width: "100vw",
+        paddingBottom: "4%",
+        zIndex: 2,
+        position: "relative",
+    }
+
+    const starsBGStyle = {
+        backgroundImage: `url(${starsBG})`,
+        backgroundColor: 'inherit',
+        backgroundRepeat: "no-repeat",
+        position: "absolute",
+        top: "0",
+        left: "0",
+        display: "flex",
+        alignItems: 'center',
+        justifyContent: "center",
+        width: "100vw",
+        height: "100vh",
+        zIndex: 1,
+    }
+
+    return (
+        <div className="App">
+            <div className="stars" style={starsBGStyle}>
+
+            </div>
+            <div className="hills" style={hillsBGStyle}>
+                <img className="icon" src={require("./images/icon-facebook.svg").default} alt="facebook icon"/>
+                <img className="icon" src={require("./images/icon-pinterest.svg").default} alt="pinterest icon"/>
+                <img className="icon" src={require("./images/icon-instagram.svg").default} alt="twitter icon"/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Counter(props){
+
+}
+
+export default Counter;
