e72723355b381e1494d66f313e08296225f30607
apodgornyitba <apodgorny@itba.edu.ar>
2024-01-17 18:39:33 -0300

    Feat: Countdown finished.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,51 @@ import './theme.css';
 import hillsBG from './images/pattern-hills.svg';
 import starsBG from './images/bg-stars.svg';
 import Counter from "./components/Counter";
+import {useEffect, useState} from "react";
 
 function App() {
+
+    const [days, setDays] = useState(2);
+    const [hours, setHours] = useState(12);
+    const [minutes, setMinutes] = useState(30);
+    const [seconds, setSeconds] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (seconds > 0) {
+                setSeconds(seconds - 1);
+            } else {
+                if (minutes > 0) {
+                    setMinutes(minutes - 1);
+                    setSeconds(59);
+                } else {
+                    if (hours > 0) {
+                        setHours(hours - 1);
+                        setMinutes(59);
+                        setSeconds(59);
+                    } else {
+                        if (days > 0) {
+                            setDays(days - 1);
+                            setHours(23);
+                            setMinutes(59);
+                            setSeconds(59);
+                        } else {
+                            clearInterval(interval);
+                        }
+                    }
+                }
+            }
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [days, hours, minutes, seconds]);
 
     const hillsBGStyle = {
         backgroundImage: `url(${hillsBG})`,
         backgroundColor: 'inherit',
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center bottom",
+        backgroundSize: "100% auto",
         display: "flex",
         alignItems: "flex-end",
         justifyContent: "center",
@@ -24,12 +61,13 @@ function App() {
         backgroundImage: `url(${starsBG})`,
         backgroundColor: 'inherit',
         backgroundRepeat: "no-repeat",
+        backgroundSize: "100% auto",
         position: "absolute",
         top: "0",
         left: "0",
         display: "flex",
+        flexDirection: "column",
         alignItems: 'center',
-        justifyContent: "center",
         width: "100vw",
         height: "100vh",
         zIndex: 1,
@@ -38,7 +76,15 @@ function App() {
     return (
         <div className="App">
             <div className="stars" style={starsBGStyle}>
-
+                <div className="starsContent">
+                    <h1>WE'RE LAUNCHING SOON</h1>
+                    <div className="counterContainer">
+                        <Counter value={days} text={"DAYS"}/>
+                        <Counter value={hours} text={"HOURS"}/>
+                        <Counter value={minutes} text={"MINUTES"}/>
+                        <Counter value={seconds} text={"SECONDS"}/>
+                    </div>
+                </div>
             </div>
             <div className="hills" style={hillsBGStyle}>
                 <img className="icon" src={require("./images/icon-facebook.svg").default} alt="facebook icon"/>
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,100 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import './Counter.css';
+import {Card} from "@mui/material";
 
-function Counter(props){
+function Counter(props) {
+    const {value, text} = props;
+    const [flip, setFlip] = useState(false);
 
+    useEffect(() => {
+        setFlip(true);
+        setTimeout(() => setFlip(false), 500);
+    }, [value]);
+
+    return (
+        <div className="counter">
+            <Card
+                sx={{
+                    backgroundColor: "var(--darker-desaturated-blue)",
+                    height: "65px",
+                    width: "160px",
+                    display: "flex",
+                    alignItems: "flex-end",
+                    justifyContent: "center",
+                    position: "absolute",
+                    overflow: "hidden",
+                    boxShadow: 'none',
+                }}
+            >
+                <div className="numberTop">{props.value}</div>
+            </Card>
+            <Card
+                sx={{
+                    backgroundColor: `${flip ? 'var(--little-darker-desaturated-blue)' :  'transparent'}`,
+                    height: "65px",
+                    width: "160px",
+                    display: "flex",
+                    alignItems: "flex-start",
+                    justifyContent: "center",
+                    overflow: "hidden",
+                    boxShadow: 'none',
+                    transform: `rotateX(${flip ? '180deg' : '0'})`,
+                    transformOrigin: 'bottom',
+                    transition: 'transform 0.5s',
+                }}
+            >
+                <div className="numberTop">{props.value}</div>
+            </Card>
+            <div className="hr-container">
+                <hr className="custom-hr"/>
+                <Card
+                    className="left-ball"
+                    sx={{
+                        backgroundColor: 'var(--very-darker-blue)',
+                        position: 'absolute',
+                        width: '10px',
+                        height: '10px',
+                        transform: 'translateY(-50%)',
+                        borderBottomRightRadius: '50%',
+                        borderTopRightRadius: '50%',
+                        borderTopLeftRadius: '0',
+                        borderBottomLeftRadius: '0',
+                        boxShadow: 'none',
+                    }}
+                />
+                <Card
+                    className="right-ball"
+                    sx={{
+                        backgroundColor: 'var(--very-darker-blue)',
+                        position: 'absolute',
+                        width: '10px',
+                        height: '10px',
+                        transform: 'translateY(-50%) translateX(1500%)',
+                        boxShadow: 'none',
+                        borderBottomRightRadius: '0',
+                        borderTopRightRadius: '0',
+                        borderTopLeftRadius: '50%',
+                        borderBottomLeftRadius: '50%',
+                    }}
+                />
+            </div>
+            <Card
+                sx={{
+                    backgroundColor: "var(--dark-desaturated-blue)",
+                    height: "65px",
+                    width: "160px",
+                    display: "flex",
+                    alignItems: "flex-start",
+                    justifyContent: "center",
+                    overflow: "hidden",
+                    boxShadow: '0 5px var(--very-darker-blue)',
+                }}
+            >
+                <div className="numberBottom">{value}</div>
+            </Card>
+            <div className='text'>{text}</div>
+        </div>
+    );
 }
 
 export default Counter;
